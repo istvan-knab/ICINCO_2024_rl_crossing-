cb50b9a22f90f33ed909b510bed245dae5fcc1a7
istvan-knab <istvan.knab@icloud.com>
2024-03-06 13:49:05 +0100

    Add basic env functionalities

---
+++ environment/traffic_environment.py
@@ -0,0 +1,14 @@
+import gymnasium as gym
+import numpy as np
+
+
+
+class TrafficEnvironment(gym.Env):
+    def __init__(self, config: dict, render: str):
+        pass
+    def step(self, action) -> None:
+        pass
+    def reset(self) -> None:
+        pass
+    def render(self) -> None:
+        pass
