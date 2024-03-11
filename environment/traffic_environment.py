00b935da2f5007fa6c40d1da5440eeae81136d8d
istvan-knab <istvan.knab@icloud.com>
2024-03-11 13:52:14 +0100

    Add pep8 fix

--- environment/traffic_environment.py
+++ environment/traffic_environment.py
@@ -9,14 +9,15 @@ import yaml
 class TrafficEnvironment(gym.Env):
     def __init__(self):
 
-        with open('env_config.yaml', 'r') as file: config = yaml.safe_load(file)
+        with open('env_config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
         number_of_intersections = "1_intersection.sumocfg"
         if config["RENDER_MODE"] == "human":
             self.render_mode = "sumo-gui"
         elif config["RENDER_MODE"] == None:
             self.render_mode = "sumo"
 
-        self.path = os.path.dirname(os.path.abspath(__file__)) + "/sumo/intersection/"  + number_of_intersections
+        self.path = os.path.dirname(os.path.abspath(__file__)) + "/sumo/intersection/" + number_of_intersections
         self.sumo_start()
 
     def sumo_start(self) -> None:
@@ -31,22 +32,25 @@ class TrafficEnvironment(gym.Env):
         else:
             sys.exit("please declare environment variable 'SUMO_HOME'")
 
-
         self.path = self.path + ""
         self.sumoCmd = [self.render_mode, "-c", self.path, "--start",
                         "--quit-on-end", "--collision.action", "remove",
                         "--no-warnings"]
         traci.start(self.sumoCmd)
         # traci.gui.setSchema("View #0", "real world")
+
     def step(self, action) -> None:
         traci.load(self.sumoCmd[1:])
+
     def reset(self) -> None:
         pass
+
     def render(self) -> None:
         """
         This function has no influence, sumo does it
         """
         pass
+
     def get_density(self):
         pass
 
