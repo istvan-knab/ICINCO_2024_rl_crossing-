9cd60e79ecca026bab6c64e47292bec48393640c
istvan-knab <istvan.knab@icloud.com>
2024-03-21 16:07:53 +0100

    Add test

--- run/test.py
+++ run/test.py
@@ -11,9 +11,11 @@ class TestTraffic:
         self.data = namedtuple('Data',
                                 ('queue_length', 'waiting_time', 'co2',
                                  'nox', 'halting_vehicles', 'arrived_vehicles'))
+        self.parameters()
         self.env = TrafficEnvironment()
-        self.action_selection = EpsilonGreedy(self, self.env)
+        self.action_selection = EpsilonGreedy(self.config, self.env)
         self.test_steps = 100
+
 
     def parameters(self) -> dict:
 
@@ -26,6 +28,10 @@ class TestTraffic:
         #self.delay_based()
         self.marl()
     def simple(self):
+
+        for warmup in range(self.env.config["WARMUP_STEPS"]):
+            traci.simulationStep()
+
         steps = self.test_steps * self.env.config["STEPS"]
         for step in range(steps):
             traci.simulationStep()
@@ -59,13 +65,14 @@ class TestTraffic:
                     state = self.env.get_state(signal)
                     state = torch.tensor(state, dtype=torch.float32, device=self.config["DEVICE"]).unsqueeze(0)
                     states.append(state)
-                    action = self.action_selection.epsilon_greedy_selection(self.model, state)
+                    action = self.action_selection.epsilon_greedy_selection(agent, state)
                     actions.append(action)
 
                 observation, reward, terminated, truncated, _ = self.env.step(actions)
+                if terminated or truncated:
+                    done = True
 
-                if done:
-                    break
+            print(episode)
 
     def log_values(self, data):
         ...
